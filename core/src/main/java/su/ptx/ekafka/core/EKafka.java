079706fddfe4f4cfe3116943d7d91e7f43459ef4
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-04-20 18:00:02 +0700

    Some debug logs

--- core/src/main/java/su/ptx/ekafka/core/EKafka.java
+++ core/src/main/java/su/ptx/ekafka/core/EKafka.java
@@ -3,6 +3,8 @@ package su.ptx.ekafka.core;
 import io.github.embeddedkafka.EmbeddedK;
 import io.github.embeddedkafka.EmbeddedKafka$;
 import io.github.embeddedkafka.EmbeddedKafkaConfigImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -10,6 +12,7 @@ import static java.util.Collections.emptyMap;
 import static su.ptx.ekafka.core.JavaToScala.immutableMap;
 
 public final class EKafka implements AutoCloseable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EKafka.class);
     private final EmbeddedK ek;
     public final String bootstrapServers;
 
@@ -40,6 +43,8 @@ public final class EKafka implements AutoCloseable {
 
     @Override
     public void close() {
+        LOGGER.debug("Stopping");
         ek.stop(true);
+        LOGGER.debug("Stopped");
     }
 }
