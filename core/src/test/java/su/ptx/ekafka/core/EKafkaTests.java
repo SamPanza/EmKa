0a8111732d5d1a825b04aba724bc69dcae1bde85
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-04-24 11:34:33 +0700

    Remove `fails_to_start_on_busy_ZK_port` test

--- core/src/test/java/su/ptx/ekafka/core/EKafkaTests.java
+++ core/src/test/java/su/ptx/ekafka/core/EKafkaTests.java
@@ -1,25 +1,13 @@
 package su.ptx.ekafka.core;
 
-import kafka.zookeeper.ZooKeeperClientTimeoutException;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.channels.ClosedByInterruptException;
-import java.nio.channels.ServerSocketChannel;
-import java.util.concurrent.CountDownLatch;
-import java.util.function.IntConsumer;
 import java.util.regex.Pattern;
 
 import static java.util.Collections.emptyMap;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EKafkaTests {
-    private static final Logger LOGGER = LoggerFactory.getLogger("ekafka.tests");
-
     @Test
     void start_and_stop() {
         try (var ek = EKafka.start(0, 0, emptyMap())) {
@@ -27,49 +15,6 @@ class EKafkaTests {
                     Pattern.compile("localhost:\\d\\d\\d\\d+")
                             .matcher(ek.bootstrapServers())
                             .matches());
-        }
-    }
-
-    @Test
-    void fails_to_start_on_busy_ZK_port() throws InterruptedException {
-        //noinspection resource
-        withBusyPort(port ->
-                assertThrows(
-                        ZooKeeperClientTimeoutException.class,
-                        () -> EKafka.start(0, port, emptyMap())));
-    }
-
-    //TODO: fails_to_start_on_busy_Kafka_port
-
-    private static void withBusyPort(IntConsumer body) throws InterruptedException {
-        try (var ssc = ServerSocketChannel.open()) {
-            ssc.bind(null);
-            LOGGER.debug("Bound to {}", ssc.getLocalAddress());
-            var latch = new CountDownLatch(1);
-            var acceptingThread = new Thread(() -> {
-                try {
-                    latch.countDown();
-                    LOGGER.debug("Waiting threads released");
-                    while (true) {
-                        ssc.accept();
-                    }
-                } catch (ClosedByInterruptException e) {
-                    //it's ok
-                } catch (IOException e) {
-                    throw new UncheckedIOException(e);
-                }
-            });
-            acceptingThread.start();
-            latch.await();
-            var port = ssc.socket().getLocalPort();
-            LOGGER.debug("Calling body.accept({})", port);
-            try {
-                body.accept(port);
-            } finally {
-                acceptingThread.interrupt();
-            }
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
         }
     }
 }
