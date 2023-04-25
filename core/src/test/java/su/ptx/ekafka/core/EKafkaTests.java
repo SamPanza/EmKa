f83cd5efaebfdb62dff404ceb692ccca8ef7abce
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-04-25 13:33:42 +0700

    Refactor tests with setUp/tearDown

--- core/src/test/java/su/ptx/ekafka/core/EKafkaTests.java
+++ core/src/test/java/su/ptx/ekafka/core/EKafkaTests.java
@@ -1,5 +1,7 @@
 package su.ptx.ekafka.core;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.regex.Pattern;
@@ -8,13 +10,23 @@ import static java.util.Collections.emptyMap;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EKafkaTests {
+    private EKafka eKafka;
+
+    @BeforeEach
+    void setUp() {
+        eKafka = EKafka.start(0, 0, emptyMap());
+    }
+
+    @AfterEach
+    void tearDown() {
+        eKafka.close();
+    }
+
     @Test
-    void start_and_stop() {
-        try (var ek = EKafka.start(0, 0, emptyMap())) {
-            assertTrue(
-                    Pattern.compile("localhost:\\d\\d\\d\\d+")
-                            .matcher(ek.bootstrapServers())
-                            .matches());
-        }
+    void bootstrapServers_looks_OK() {
+        assertTrue(
+                Pattern.compile("localhost:\\d\\d\\d\\d+")
+                        .matcher(eKafka.bootstrapServers())
+                        .matches());
     }
 }
