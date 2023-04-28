ec47e2db49a2b35df48b86cd0432e3f78ced0b7f
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-04-28 23:07:46 +0700

    Continue experimenting with Mockito extension API

--- junit/src/main/java/su/ptx/emka/junit/EmKaExtension.java
+++ junit/src/main/java/su/ptx/emka/junit/EmKaExtension.java
@@ -3,8 +3,10 @@ package su.ptx.emka.junit;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
 
-public class EmKaExtension implements BeforeEachCallback, AfterEachCallback {
+public class EmKaExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
     @Override
     public void beforeEach(ExtensionContext ctx) {
         System.err.println("beforeEach");
@@ -13,5 +15,15 @@ public class EmKaExtension implements BeforeEachCallback, AfterEachCallback {
     @Override
     public void afterEach(ExtensionContext ctx) {
         System.err.println("afterEach");
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext p, ExtensionContext e) {
+        return p.isAnnotated(BootstrapServers.class);
+    }
+
+    @Override
+    public String resolveParameter(ParameterContext p, ExtensionContext e) {
+        return "ачо?";
     }
 }
--- junit/src/test/java/su/ptx/emka/junit/alien/EmKaExtensionOnParameterTests.java
+++ junit/src/test/java/su/ptx/emka/junit/alien/EmKaExtensionOnParameterTests.java
@@ -2,12 +2,14 @@ package su.ptx.emka.junit.alien;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import su.ptx.emka.junit.BootstrapServers;
 import su.ptx.emka.junit.EmKaExtension;
 
+@ExtendWith(EmKaExtension.class)
 class EmKaExtensionOnParameterTests {
     @Test
-    void test1(@ExtendWith(EmKaExtension.class) String foo) {
-        System.err.println("test1:" + foo);
+    void test1(@BootstrapServers String bServers) {
+        System.err.println("test1:" + bServers);
     }
 
     @Test
