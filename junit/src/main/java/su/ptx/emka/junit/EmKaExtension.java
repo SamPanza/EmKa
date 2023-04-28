5baa230e42c239785816eef7eb476807c63302ed
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-04-28 18:22:03 +0700

    Here we go with EmKa JUnit 5 extension

---
+++ junit/src/main/java/su/ptx/emka/junit/EmKaExtension.java
@@ -0,0 +1,17 @@
+package su.ptx.emka.junit;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class EmKaExtension implements BeforeEachCallback, AfterEachCallback {
+    @Override
+    public void afterEach(ExtensionContext extensionContext) {
+        //TODO
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext extensionContext) {
+        //TODO
+    }
+}
