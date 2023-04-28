54f5c56db1af677cece2a00d784aac82caf7916e
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-04-28 13:54:20 +0700

    Restructure KRaftee internals by introducing companion object & move static stuff there

--- core/src/main/java/su/ptx/emka/core/KRaftee.java
+++ core/src/main/java/su/ptx/emka/core/KRaftee.java
@@ -6,6 +6,7 @@ import kafka.server.KafkaBroker;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaRaftServer;
 import kafka.server.MetaProperties;
+import kafka.tools.StorageTool;
 import org.apache.kafka.common.Uuid;
 import org.apache.kafka.common.network.ListenerName;
 import org.apache.kafka.common.utils.Time;
@@ -13,9 +14,11 @@ import org.apache.kafka.metadata.bootstrap.BootstrapDirectory;
 import org.apache.kafka.metadata.bootstrap.BootstrapMetadata;
 import org.apache.kafka.server.common.MetadataVersion;
 import scala.Option;
+import scala.collection.immutable.Seq;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.nio.file.Files;
@@ -24,32 +27,12 @@ import java.util.Optional;
 import java.util.function.IntSupplier;
 
 final class KRaftee implements EmKa {
-    private static final int NODE_ID = 1;
-
-    private static String q_voters(int port) {
-        return NODE_ID + "@" + host_port(port);
-    }
-
-    private static final String CTL = "CTL";
-    private static final String BRO = "BRO";
-
-    private static String lstnr(String name, int port) {
-        return name + "://" + host_port(port);
-    }
-
-    private static final String HOST = "localhost";
-
-    private static String host_port(int port) {
-        return HOST + ":" + port;
-    }
-
     private final KafkaRaftServer kafka;
     private final KafkaBroker bro;
 
     KRaftee() throws Exception {
         kafka = new KafkaRaftServer(
-                new KafkaConfig(
-                        newProps(newLogDir()), false),
+                Co.kafkaConfig(),
                 Time.SYSTEM,
                 Option.empty());
         //
@@ -63,7 +46,7 @@ final class KRaftee implements EmKa {
 
     @Override
     public String bootstrapServers() {
-        return host_port(bro.boundPort(new ListenerName(BRO)));
+        return Co.host_port(bro.boundPort(new ListenerName(Co.BRO)));
     }
 
     @Override
@@ -79,45 +62,73 @@ final class KRaftee implements EmKa {
         return this;
     }
 
-    private static Map<?, ?> newProps(File logDir) {
-        IntSupplier port0 = () -> {
-            try (var s = new ServerSocket(0)) {
-                return s.getLocalPort();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        };
-        var cp = port0.getAsInt();
-        var bp = port0.getAsInt();
-        short rf = 1;
-        return Map.of(
-                "process.roles", "controller,broker",
-                "node.id", NODE_ID,
-                "controller.quorum.voters", q_voters(cp),
-                "listeners", lstnr(CTL, cp) + "," + lstnr(BRO, bp),
-                "listener.security.protocol.map", "%s:PLAINTEXT,%s:PLAINTEXT".formatted(CTL, BRO),
-                "controller.listener.names", CTL,
-                "inter.broker.listener.name", BRO,
-                "log.dir", logDir.toString(),
-                "offsets.topic.replication.factor", rf,
-                "transaction.state.log.replication.factor", rf);
-    }
+    private static final class Co {
+        private static KafkaConfig kafkaConfig() throws Exception {
+            return new KafkaConfig(
+                    serverProps(
+                            formatTmpLogDir()),
+                    false);
+        }
 
-    private static File newLogDir() throws Exception {
-        var logDir = Files.createTempDirectory("emka").toFile();
-        logDir.deleteOnExit();
+        private static Map<?, ?> serverProps(File logDir) {
+            IntSupplier port_0 = () -> {
+                try (var s = new ServerSocket(0)) {
+                    return s.getLocalPort();
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            };
+            var cp = port_0.getAsInt();
+            var bp = port_0.getAsInt();
+            short rf = 1;
+            return Map.of(
+                    "process.roles", "controller,broker",
+                    "node.id", NODE_ID,
+                    "controller.quorum.voters", q_voters(cp),
+                    "listeners", lstnr(CTL, cp) + "," + lstnr(BRO, bp),
+                    "listener.security.protocol.map", "%s:PLAINTEXT,%s:PLAINTEXT".formatted(CTL, BRO),
+                    "controller.listener.names", CTL,
+                    "inter.broker.listener.name", BRO,
+                    "log.dir", logDir.toString(),
+                    "offsets.topic.replication.factor", rf,
+                    "transaction.state.log.replication.factor", rf);
+        }
 
-        new BrokerMetadataCheckpoint(new File(logDir, "meta.properties")).write(
-                new MetaProperties(Uuid.randomUuid().toString(), NODE_ID).toProperties());
+        /**
+         * See {@link StorageTool#formatCommand(PrintStream, Seq, MetaProperties, MetadataVersion, boolean)}
+         */
+        private static File formatTmpLogDir() throws Exception {
+            var tld = Files.createTempDirectory(null).toFile();
+            tld.deleteOnExit();
 
-        new BootstrapDirectory(
-                logDir.toString(),
-                Optional.empty())
-                .writeBinaryFile(
-                        BootstrapMetadata.fromVersion(
-                                MetadataVersion.latest(),
-                                "emka"));
+            new BrokerMetadataCheckpoint(new File(tld, "meta.properties")).write(
+                    new MetaProperties(Uuid.randomUuid().toString(), NODE_ID).toProperties());
 
-        return logDir;
+            new BootstrapDirectory(
+                    tld.toString(),
+                    Optional.empty())
+                    .writeBinaryFile(
+                            BootstrapMetadata.fromVersion(
+                                    MetadataVersion.latest(),
+                                    "kraftee-emka"));
+            return tld;
+        }
+
+        private static final int NODE_ID = 1;
+        private static final String HOST = "localhost";
+        private static final String CTL = "CTL";
+        private static final String BRO = "BRO";
+
+        private static String q_voters(int port) {
+            return NODE_ID + "@" + host_port(port);
+        }
+
+        private static String lstnr(String name, int port) {
+            return name + "://" + host_port(port);
+        }
+
+        private static String host_port(int port) {
+            return HOST + ":" + port;
+        }
     }
 }
