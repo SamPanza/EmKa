7e833c281c3d317587abbcae672c8314e6c16a8a
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-05-05 14:23:13 +0700

    App's HERE!

--- app/src/main/java/su/ptx/emka/app/App.java
+++ app/src/main/java/su/ptx/emka/app/App.java
@@ -1,14 +1,23 @@
 package su.ptx.emka.app;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import su.ptx.emka.core.KRaftee;
+
+import java.io.File;
 
 import static java.lang.System.out;
 
 public class App {
     public static void main(String[] args) {
+        var config = ConfigProvider.getConfig();
+        int brop = config.getOptionalValue("brop", int.class).orElse(0);
+        int conp = config.getOptionalValue("conp", int.class).orElse(0);
+        File logd = config.getOptionalValue("logd", File.class).orElse(null);
+        out.format("brop=%d, conp=%d, logd=%s\n", brop, conp, logd);
+
         @SuppressWarnings("resource")
         var k = new KRaftee();
-        k.start(0, 0, null);
+        k.start(brop, conp, logd);
         out.println(k.bootstrapServers());
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             k.stop();
