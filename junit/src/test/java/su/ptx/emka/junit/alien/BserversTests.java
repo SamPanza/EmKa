f1593748837087b60e7a2ca95526c374d6b944f6
SamPanza <49244874+SamPanza@users.noreply.github.com>
2023-05-17 03:20:50 +0700

    BserversTests as example

---
+++ junit/src/test/java/su/ptx/emka/junit/alien/BserversTests.java
@@ -0,0 +1,20 @@
+package su.ptx.emka.junit.alien;
+
+import org.junit.jupiter.api.Test;
+import su.ptx.emka.junit.BootstrapServers;
+import su.ptx.emka.junit.EmKa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@EmKa
+class BserversTests {
+    @BootstrapServers
+    private String bservers1;
+
+    @Test
+    void bservers(@BootstrapServers String bservers2) {
+        assertTrue(bservers1.matches("localhost:\\d{4,}"));
+        assertEquals(bservers1, bservers2);
+    }
+}
