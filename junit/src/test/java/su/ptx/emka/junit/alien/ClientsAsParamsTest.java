388d955f2ef4227b08ca5c40c8ba0d74b73d4b65
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-05-16 14:12:07 +0700

    Create topic before producing/consuming in clients' test

--- junit/src/test/java/su/ptx/emka/junit/alien/ClientsAsParamsTest.java
+++ junit/src/test/java/su/ptx/emka/junit/alien/ClientsAsParamsTest.java
@@ -1,28 +1,42 @@
 package su.ptx.emka.junit.alien;
 
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.extension.ExtendWith;
 import su.ptx.emka.junit.EmKaExtension;
 import su.ptx.emka.junit.Konsumer;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import static java.time.Duration.ofSeconds;
+import static java.util.Collections.singleton;
+import static java.util.Optional.empty;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(EmKaExtension.class)
 class ClientsAsParamsTest {
     private static final String topic = "1";
 
+    @BeforeEach
+    void beforeEach(Admin admin, TestReporter tr) throws ExecutionException, InterruptedException {
+        var ctr = admin.createTopics(singleton(new NewTopic(topic, empty(), empty())));
+        tr.publishEntry("numPartitions", ctr.numPartitions(topic).get().toString());
+        tr.publishEntry("replicationFactor", ctr.replicationFactor(topic).get().toString());
+    }
+
     @Test
-    @ExtendWith(EmKaExtension.class)
     @DisplayName("consume & produce")
     void test(Producer<Integer, Integer> p,
-              @Konsumer(subsribeTo = topic) Consumer<Integer, Integer> c) throws InterruptedException {
+              @Konsumer(group = "1", subsribeTo = topic) Consumer<Integer, Integer> c) throws InterruptedException {
         var sent = Set.of(1, 2, 3, 4, 5);
         Set<Integer> received = new HashSet<>();
         var poll = new Thread(() -> {
