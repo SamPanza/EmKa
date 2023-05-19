e26be40ade664f4476792e476112d45559e04d58
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-05-19 13:24:41 +0700

    Fix import order

--- app/src/main/java/su/ptx/emka/app/App.java
+++ app/src/main/java/su/ptx/emka/app/App.java
@@ -1,7 +1,8 @@
 package su.ptx.emka.app;
 
-import java.io.File;
+import static java.lang.System.out;
 
+import java.io.File;
 import org.eclipse.microprofile.config.ConfigProvider;
 import su.ptx.emka.core.EmKaServer;
 
@@ -17,15 +18,15 @@ public class App {
     int brop = config.getOptionalValue("brop", int.class).orElse(9092);
     int conp = config.getOptionalValue("conp", int.class).orElse(9093);
     File logd = config.getOptionalValue("logd", File.class).orElse(new File("/tmp/emka"));
-    System.out.format("brop=%d, conp=%d, logd=%s\n", brop, conp, logd);
+    out.format("brop=%d, conp=%d, logd=%s\n", brop, conp, logd);
 
     @SuppressWarnings("resource")
     var emKaServer = EmKaServer.create();
     emKaServer.run(brop, conp, logd);
-    System.out.println(emKaServer.bootstrapServers());
+    out.println(emKaServer.bootstrapServers());
     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
       emKaServer.close();
-      System.out.println("See ya!");
+      out.println("See ya!");
     }));
   }
 }
