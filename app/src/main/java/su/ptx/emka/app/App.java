8a9f4e98a869386676c95573ea59cef93ebad04c
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-05-19 11:57:41 +0700

    Indent XMLs with two spaces & force it with EditorConfig file & plugin; Gradually follow `google_checks` with Checkstyle

--- app/src/main/java/su/ptx/emka/app/App.java
+++ app/src/main/java/su/ptx/emka/app/App.java
@@ -1,27 +1,30 @@
 package su.ptx.emka.app;
 
+import java.io.File;
 import org.eclipse.microprofile.config.ConfigProvider;
 import su.ptx.emka.core.EmKaServer;
 
-import java.io.File;
-
-import static java.lang.System.out;
-
+/**
+ * EmKa standalone application.
+ */
 public class App {
-    public static void main(String[] args) {
-        var config = ConfigProvider.getConfig();
-        int brop = config.getOptionalValue("brop", int.class).orElse(9092);
-        int conp = config.getOptionalValue("conp", int.class).orElse(9093);
-        File logd = config.getOptionalValue("logd", File.class).orElse(new File("/tmp/emka"));
-        out.format("brop=%d, conp=%d, logd=%s\n", brop, conp, logd);
+  /**
+   * main.
+   */
+  public static void main(String[] args) {
+    var config = ConfigProvider.getConfig();
+    int brop = config.getOptionalValue("brop", int.class).orElse(9092);
+    int conp = config.getOptionalValue("conp", int.class).orElse(9093);
+    File logd = config.getOptionalValue("logd", File.class).orElse(new File("/tmp/emka"));
+    System.out.format("brop=%d, conp=%d, logd=%s\n", brop, conp, logd);
 
-        @SuppressWarnings("resource")
-        var emKaServer = EmKaServer.create();
-        emKaServer.run(brop, conp, logd);
-        out.println(emKaServer.bootstrapServers());
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            emKaServer.close();
-            out.println("See ya!");
-        }));
-    }
+    @SuppressWarnings("resource")
+    var emKaServer = EmKaServer.create();
+    emKaServer.run(brop, conp, logd);
+    System.out.println(emKaServer.bootstrapServers());
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      emKaServer.close();
+      System.out.println("See ya!");
+    }));
+  }
 }
