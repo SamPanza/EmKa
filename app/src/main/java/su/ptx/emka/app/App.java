625d30e38b809e0eee240abb6ba49b3500e816eb
SamPanza <49244874+SamPanza@users.noreply.github.com>
2023-05-19 22:15:38 +0700

    Refuck deps & scopes; app deps are core, mp cfg, logback-classic (rt)

--- app/src/main/java/su/ptx/emka/app/App.java
+++ app/src/main/java/su/ptx/emka/app/App.java
@@ -1,33 +1,32 @@
 package su.ptx.emka.app;
 
-import static java.lang.Runtime.getRuntime;
-import static java.lang.System.out;
-
-import java.io.File;
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import su.ptx.emka.core.EmKaServer;
+
+import java.io.File;
+
+import static java.lang.Runtime.getRuntime;
+import static java.lang.System.out;
 
 /**
  * EmKa standalone application.
  */
 public class App {
+  private static final Config cfg = ConfigProvider.getConfig();
+  private static final int port1 = cfg.getOptionalValue("emka.bro.port", int.class).orElse(0);
+  private static final int port2 = cfg.getOptionalValue("emka.con.port", int.class).orElse(0);
+  private static final File logDir = cfg.getOptionalValue("emka.log.dir", File.class).orElse(null);
+
   /**
    * main.
    *
    * @param args Not used
    */
   public static void main(String[] args) {
-    var config = ConfigProvider.getConfig();
-    int b = config.getOptionalValue("b", int.class).orElse(9092);
-    int c = config.getOptionalValue("c", int.class).orElse(9093);
-    //TODO: Empty string
-    File d = config.getOptionalValue("d", File.class).orElse(new File("/tmp/emka"));
-    out.format("%d %d %s\n", b, c, d);
-
-    @SuppressWarnings("resource")
     var emKaServer = EmKaServer.create();
-    emKaServer.run(b, c, d);
-    out.println(emKaServer.bootstrapServers());
+    emKaServer.run(port1, port2, logDir);
+    out.println(emKaServer);
 
     getRuntime().addShutdownHook(new Thread(() -> {
       emKaServer.close();
