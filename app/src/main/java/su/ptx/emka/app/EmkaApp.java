33604648206adca4795499a3e2b7b3f90b3f2893
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-05-23 16:01:25 +0700

    Make app an MBean so it ready to be started / stopped by Spring Boot plugin

---
+++ app/src/main/java/su/ptx/emka/app/EmkaApp.java
@@ -0,0 +1,53 @@
+package su.ptx.emka.app;
+
+import static java.lang.Runtime.getRuntime;
+import static java.lang.management.ManagementFactory.getPlatformMBeanServer;
+
+import java.io.File;
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+import org.eclipse.microprofile.config.ConfigProvider;
+import su.ptx.emka.core.EmKaServer;
+
+/**
+ * EmKa standalone application.
+ */
+public class EmkaApp implements Runnable, EmkaAppMBean {
+  /**
+   * main.
+   */
+  public static void main(String[] args) throws MalformedObjectNameException, NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException {
+    new EmkaApp().run();
+  }
+
+  private final EmKaServer server;
+
+  private EmkaApp() throws MalformedObjectNameException, NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException {
+    server = EmKaServer.create();
+    getRuntime().addShutdownHook(new Thread(this::shutdown));
+    var c = getClass();
+    getPlatformMBeanServer().registerMBean(this, new ObjectName("%s:type=%s".formatted(c.getPackageName(), c.getSimpleName())));
+  }
+
+  @Override
+  public void run() {
+    var cfg = ConfigProvider.getConfig();
+    server.run(
+      cfg.getOptionalValue("emka.bro.port", int.class).orElse(0),
+      cfg.getOptionalValue("emka.con.port", int.class).orElse(0),
+      cfg.getOptionalValue("emka.log.dir", File.class).orElse(null));
+  }
+
+  @Override
+  public boolean isReady() {
+    return server.bootstrapServers() != null;
+  }
+
+  @Override
+  public void shutdown() {
+    server.close();
+  }
+}
---
+++ app/src/main/java/su/ptx/emka/app/EmkaAppMBean.java
@@ -0,0 +1,7 @@
+package su.ptx.emka.app;
+
+public interface EmkaAppMBean {
+  boolean isReady();
+
+  void shutdown();
+}
