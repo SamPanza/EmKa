4cb18254dc0e5aad5c654d7df9e05a4c20f428b2
Sam Panza <49244874+SamPanza@users.noreply.github.com>
2023-06-02 12:04:13 +0700

    #11 Inspect running app quorum info & log dirs in IT

--- app/src/test/java/su/ptx/emka/app/EmKaAppIT.java
+++ app/src/test/java/su/ptx/emka/app/EmKaAppIT.java
@@ -1,9 +1,15 @@
 package su.ptx.emka.app;
 
+import static java.util.Collections.singleton;
+import static java.util.Collections.singletonMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static su.ptx.emka.app.Jmx.attr;
 import static su.ptx.emka.app.Jmx.withConnection;
 
-import java.util.Map;
+import org.apache.kafka.clients.admin.Admin;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestReporter;
 
@@ -11,11 +17,30 @@ class EmKaAppIT {
   @Test
   void inspect(TestReporter tr) {
     withConnection(9001, c -> {
-      tr.publishEntry(
-          Map.of(
-              "bootstrap.servers", attr(c, EmKaApp.class, "BootstrapServers"),
-              "log.dir", attr(c, EmKaApp.class, "LogDir")));
-      //TODO: Connect client
+      String bservers = attr(c, EmKaApp.class, "BootstrapServers");
+      tr.publishEntry("bootstrap.servers", bservers);
+      String ldir = attr(c, EmKaApp.class, "LogDir");
+      tr.publishEntry("log.dir", ldir);
+      try (var admin = Admin.create(singletonMap("bootstrap.servers", bservers))) {
+        admin.describeMetadataQuorum().quorumInfo().whenComplete((qi, e) -> {
+          assertNotNull(qi);
+          assertNull(e);
+          assertEquals(1, qi.leaderId());
+          assertEquals(1, qi.leaderEpoch());
+          tr.publishEntry("highWatermark", String.valueOf(qi.highWatermark()));
+          var voters = qi.voters();
+          assertEquals(1, voters.size());
+          assertEquals(1, voters.get(0).replicaId());
+          assertTrue(qi.observers().isEmpty());
+        });
+        admin.describeLogDirs(singleton(1)).allDescriptions().whenComplete((m, e) -> {
+          assertNotNull(m);
+          assertNull(e);
+          var ldd1 = m.get(1);
+          assertEquals(1, ldd1.size());
+          assertEquals(ldir, ldd1.keySet().iterator().next());
+        });
+      }
     });
   }
 }
